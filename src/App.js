0f008de096c708e917ca2aa77d619c19c0e9ed1a
sophie <sneuville59@gmail.com>
2020-07-08 18:14:59 +0200

    add handlechange and handlesubmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SignUp from './components/SignUp';
+
 import "./App.css";
 
 function App() {
   return <div className="App">
     <SignUp/>
+   
   </div>;
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,20 +1,75 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
 
 
-export default function SignUp() {
-    const[email, setEmail] = useState("")
+const SignUp = () => {
+  const [newUser, setNewUser] = useState({});
 
-    
-  return <div >
-       <form >
-        <label>
-          Sign Up:
-          <input value={email} onChange={e => setEmail(e.target.value)}  />
-        </label>
+  const UserChange = (e) => {
+    const tmp = { ...newUser, [e.target.name]: e.target.value };
+    setNewUser(tmp);
+  };
+const json= JSON.stringify(newUser)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newUser)
+  };
+
+  return (
+    <div>
+      <h1>{json}</h1>
+      <h2>Inscris-toi</h2>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          onChange={(e) => {
+            UserChange(e);
+          }}
+          placeholder="Ton Nom"
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          onChange={(e) => {
+            UserChange(e);
+          }}
+          placeholder="Ton Email"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          onChange={(e) => {
+            UserChange(e);
+          }}
+          placeholder="Ton mot de passe"
+        />
+        <input
+          type="password"
+          name="passwordbis"
+          id="passwordbis"
+          required
+          
+          onChange={(e) => {
+            UserChange(e);
+          }}
+          placeholder="Confirme ton mot de passe"
+        />
+        <button type="submit" value="Je m'inscris" >Je m'inscris</button>
       </form>
-      <h1>{email}</h1>
-  </div>;
-}
-
+    </div>
+  );
+};
+export default SignUp;
